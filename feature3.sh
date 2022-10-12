8c8bd806e4699986411c77eb58c3d7a28c8527c3
Thais Lourenço <thaislou21@gmail.com>
2022-10-12 20:25:07 -0300

    feature3

---
+++ feature3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "O voo com maior atraso na saída foi:"
+
+# Pega apenas colunas necessárias para a análise (número do voo e ArrDelay)
+cat 2006-sample.csv | cut -d"," -f10,16 |
+# Remove o cabeçalho
+tail -n +2 |
+# Inverte as colnas
+awk -F ',' '{ print $2 "," $1}' |
+# Ordena do maior pro menor
+sort -n |
+# Pegar maior valor
+tail -n1 |
+# Pega apenas colunas necessárias para a análise (número do voo)
+cut -d"," -f2
