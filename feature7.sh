360d35d1c0af7426e71a55c8185b26300714fa4a
Tamires Brito <tamires.brito10@gmail.com>
2022-10-13 12:57:28 -0300

    feature7

--- feature7.sh
+++ feature7.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+# Filtrando as colunas de interesse (5, 6 e 17)
+cat 2006-sample.csv | cut -d"," -f5,6,17 |
+
+# Filtrando apenas vôos saídos de JFK
+grep JFK |
+
+# Subtraindo o horário de saída pelo horário de saída previsto
+awk -F ',' '{ print $0 "," $1 - $2 }' |
+
+# Tirando os horários negativos
+grep -v - |
+
+# Deixando apenas a coluna referente a atrasos de saída de JFK
+cut -d"," -f4 |
+
+# Somando tempo de atraso de saída de JFK
+paste -sd+ | bc 
